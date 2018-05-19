2685c83f4919e4374d89059483bef4f963bf4727
Wagner Silvestre <wagner1343@outlook.com>
2018-05-19 16:46:59 -0300

    .

--- src/trabalhopoo/Main.java
+++ src/trabalhopoo/Main.java
@@ -13,8 +13,8 @@ public class Main {
     public static void main(String[] args) {
         String palavra = ConjuntoPalavras.EscolhaPalavra(0);
         Jogo jogo = EscolheMetodo.Escolhe(1);
-        String novaPalavra = jogo.Muda(palavra);
 
+        String novaPalavra = jogo.Muda(palavra);
         System.out.println(novaPalavra);
     }
 }
